0fea07e4cbe4c24a449172d49cda2245e53d22b1
gimura2022 <gimura0001@gmail.com>
2024-12-05 19:54:47 +0300

    fixed gstd__allocator and gstd__deallocator names and added gstd__memmanager structure

--- include/allocators.h
+++ include/allocators.h
@@ -3,7 +3,12 @@
 
 #include <stddef.h>
 
-typedef void *(*gstd__allocator)(size_t);
-typedef void  (*gstd__deallocator)(void*);
+typedef void *(*gstd__allocator_t)(size_t);
+typedef void  (*gstd__deallocator_t)(void*);
+
+struct gstd__memmanager {
+	gstd__allocator_t allocator;
+	gstd__deallocator_t deallocator;
+};
 
 #endif
