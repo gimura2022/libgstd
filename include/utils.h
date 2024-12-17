86d25888eb7a207dafc3c953661e46c432102cc2
gimura2022 <gimura0001@gmail.com>
2024-12-17 17:56:54 +0300

    updated utils.h

--- include/utils.h
+++ include/utils.h
@@ -1,6 +1,11 @@
 #ifndef _utils_h
 #define _utils_h
 
+#include <stdbool.h>
+
 #define array_size(x) sizeof(x) / sizeof(x[0])
+
+#define continue_or_retrun(x) ({ if (!x) return false; })
+#define continue_break_or_retrun(x) ({ if (!x) return false; break; })
 
 #endif
